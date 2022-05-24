f93198cd10776cc24d72da1629e9f2d379a70261
serhiiarkhypiuk <airborn.gnus@gmail.com>
2022-05-24 21:41:23 +0300

    vimeo player added

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from "@vimeo/player"
+import throttle from "lodash/throttle"
 
+const iframe = document.querySelector('iframe')
+const player = new Player(iframe)
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
+
+player.on('timeupdate', throttle((playTime => {
+    localStorage.setItem("videoplayer-current-time", playTime.seconds)
+}), 1000))
