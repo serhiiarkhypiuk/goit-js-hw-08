24b919becdf0ce835e9f04cca1132ee0b37daeed
serhiiarkhypiuk <airborn.gnus@gmail.com>
2022-05-24 21:57:40 +0300

    updated default time value

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,6 +4,7 @@ import throttle from "lodash/throttle"
 const iframe = document.querySelector('iframe')
 const player = new Player(iframe)
 
+localStorage.setItem("videoplayer-current-time", 0)
 player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
 
 player.on('timeupdate', throttle((playTime => {
