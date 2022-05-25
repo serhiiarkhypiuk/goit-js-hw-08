6df72374147b2e91a7b349b0391e7008d8aeda3f
serhiiarkhypiuk <airborn.gnus@gmail.com>
2022-05-25 11:32:01 +0300

    feedback form added

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,15 @@
+import throttle from "lodash/throttle"
+
+const form = document.querySelector('form.feedback-form')
+
+const updateStorage = (event) => {
+    const {
+        elements: { email, message }
+    } = event.currentTarget
+
+    localStorage.setItem("feedback-form-state",
+        JSON.stringify({ [email.name]: email.value, [message.name]: message.value }))
+}
+
+form.addEventListener('input', throttle(updateStorage, 500))
 
